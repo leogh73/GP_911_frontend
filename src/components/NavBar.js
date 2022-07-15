b3f4d93d17652773b293aaf7f87612508e69e974
LEO <leocuevas73@gmail.com>
2022-07-15 15:22:53 -0300

    dark mode toggler

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { IconContext } from 'react-icons';
@@ -20,6 +20,9 @@ const NavBar = () => {
 	const context = useContext(UserContext);
 	const sideBarActive = useRef(false);
 	const navigate = useNavigate();
+	const [darkMode, setDarkMode] = useState(
+		localStorage.getItem('mode') == 'dark-mode' ? true : false,
+	);
 	const logout = () => {
 		context.logout(false);
 		navigate('/');
@@ -34,7 +37,6 @@ const NavBar = () => {
 		const toggleNavBar = () => {
 			sideBarActive.current = !sideBarActive.current;
 
-			console.log(sideBarActive.current);
 			nav.classList.toggle('nav-active');
 			navLinks.forEach((link, index) => {
 				if (link.style.animation === '') {
@@ -45,21 +47,52 @@ const NavBar = () => {
 			center.classList.toggle('body-overlay');
 		};
 
-		burger.addEventListener('click', () => toggleNavBar());
+		if (context.token) {
+			burger.addEventListener('click', () => toggleNavBar());
 
-		center.addEventListener('click', () => {
-			if (nav.classList.value === 'nav-links nav-active') toggleNavBar();
+			center.addEventListener('click', () => {
+				if (nav.classList.value === 'nav-links nav-active') toggleNavBar();
+			});
+		}
+
+		const body = document.querySelector('body');
+		const modeToggle = document.querySelector('.dark-light');
+
+		if (darkMode) body.classList.add('dark');
+
+		modeToggle.addEventListener('click', () => {
+			body.classList.toggle('dark');
+			if (!body.classList.contains('dark')) {
+				localStorage.setItem('mode', 'light-mode');
+			} else {
+				localStorage.setItem('mode', 'dark-mode');
+			}
 		});
 
 		return () => {
-			burger.removeEventListener('click', () => {
-				nav.classList.toggle('nav-links');
-			});
-			center.removeEventListener('click', () => {
-				if (nav.classList.value === 'nav-links nav-active') toggleNavBar();
-			});
+			if (context.token) {
+				burger.removeEventListener('click', () => {
+					nav.classList.toggle('nav-links');
+				});
+				center.removeEventListener('click', () => {
+					if (nav.classList.value === 'nav-links nav-active') toggleNavBar();
+				});
+				modeToggle.removeEventListener('click', () => {});
+			}
 		};
-	}, [sideBarActive]);
+	}, [context.token]);
+
+	const modeButton = () => (
+		<>
+			<div className="dark-light" onClick={() => setDarkMode(!darkMode)}>
+				{darkMode ? (
+					<FiSun className="dark-light-icon" size={25} />
+				) : (
+					<FiMoon className="dark-light-icon" size={25} />
+				)}
+			</div>
+		</>
+	);
 
 	return (
 		<IconContext.Provider
@@ -67,41 +100,43 @@ const NavBar = () => {
 				style: { marginRight: '12px', marginBottom: '3px' },
 			}}
 		>
-			<nav>
+			<nav className="nav-color">
 				<div className="logo">
 					<h4>Guardias 911</h4>
 				</div>
-				<ul className="nav-links">
-					<li>
-						<a href="#">
-							<FaRegCalendarAlt />
-							Cronograma
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<FaList />
-							Cambios
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<FaExchangeAlt />
-							Nuevo cambio
-						</a>
-					</li>
-				</ul>
-				<div className="icon">
-					<FiSun />
-				</div>
-				<div className="icon">
-					<FaUserAlt />
-				</div>
-				<div className="burger">
-					<div className="line1"></div>
-					<div className="line2"></div>
-					<div className="line3"></div>
-				</div>
+				{context.token ? (
+					<>
+						<ul className="nav-links">
+							<li>
+								<a href="#">
+									<FaRegCalendarAlt />
+									Cronograma
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<FaList />
+									Cambios
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<FaExchangeAlt />
+									Nuevo cambio
+								</a>
+							</li>
+						</ul>
+						{modeButton()}
+						<FaUserAlt className="user-icon" size={20} />
+						<div className="burger">
+							<div className="line1"></div>
+							<div className="line2"></div>
+							<div className="line3"></div>
+						</div>
+					</>
+				) : (
+					modeButton()
+				)}
 			</nav>
 		</IconContext.Provider>
 	);
